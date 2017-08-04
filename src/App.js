0c1ead1e0cbd7bfeb0154a553aaff6c38c034ad3
EgimenoS <ergisan@gmail.com>
2017-08-04 15:26:22 +0000

    created a helper to generate urls consumed by axios

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
-import axios from 'axios'
+import axios from 'axios';
+import urlFor from './helpers/urlFor';
 
 class App extends Component {
 	constructor() {
@@ -21,7 +22,7 @@ class App extends Component {
 	}
 
 	getNotes = () => {
-		axios.get('https://notes-ernesto.herokuapp.com/notes')
+		axios.get(urlFor('notes'))
 		.then((res) => console.log(res.data))
 		.catch((err) => console.log(err.response.data));
 	}
---
+++ src/helpers/urlFor.js
@@ -0,0 +1,5 @@
+const urlFor = (endpoint) => {
+    return ' https://notes-ernesto.herokuapp.com/' + endpoint;
+};
+
+export default urlFor;
