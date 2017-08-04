473eb21e4b1b99009a6afe54341144dadfb57f30
EgimenoS <ergisan@gmail.com>
2017-08-04 15:12:07 +0000

    added axios and first GET promise

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
+import axios from 'axios'
 
 class App extends Component {
 	constructor() {
@@ -19,12 +20,18 @@ class App extends Component {
 		
 	}
 
+	getNotes = () => {
+		axios.get('https://notes-ernesto.herokuapp.com/notes')
+		.then((res) => console.log(res.data))
+		.catch((err) => console.log(err.response.data));
+	}
+
 	render() {
-		const { showNote } = this.state; /*object sestructuring, equivalent to this.state.showNote <p>{this.state.showNote</p> */
+		const { showNote } = this.state; /*object destructuring, equivalent to this.state.showNote <p>{this.state.showNote</p> */
 		return (
 		<div className="App">
-			<Nav toggleNote={this.toggleNote} showNote={showNote} />
-			{ showNote ? <Note /> : <List /> }
+			<Nav toggleNote={this.toggleNote} showNote={showNote} /> 
+			{ showNote ? <Note /> : <List getNotes={this.getNotes} /> }
 		</div>
 		);
 	}
