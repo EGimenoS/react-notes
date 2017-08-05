788dbcf440b04242a98b6f73d5488c37e4f3e160
EgimenoS <ergisan@gmail.com>
2017-08-05 18:17:35 +0000

    POST request to create new notes

--- src/App.js
+++ src/App.js
@@ -36,13 +36,21 @@ class App extends Component {
 		.catch((err) => console.log(err.response.data))
 	}
 
+	submitNote = (data) => {
+		axios.post(urlFor('notes'), data)
+		.then((res) => this.setState({ showNote: false }))
+		.catch((err) => console.log(err.response.data));
+	}
+
 	render() {
 		const { showNote, notes, note } = this.state; /*object destructuring, equivalent to this.state.showNote <p>{this.state.showNote</p> */
 		return (
 		<div className="App">
 			<Nav toggleNote={this.toggleNote} showNote={showNote} /> 
 			{ showNote ? 
-				<Note note={note}
+				<Note 
+					note={note}
+					submitNote={this.submitNote}
 				/> 
 				: 
 				<List 
